012decc7d4f13f6a57bcdc78126bc5524ed26b8d
depp <safescanning@gmail.com>
2026-05-19 10:26:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown("""
     /* Hintergrund: Cyan */
     .stApp {
         background: radial-gradient(circle at top right, #1e293b, #0f172a, #020617);
-        color: #e0ffff;
+        color: #00FFFF;
     }
     
     /* SideBar Styling */
